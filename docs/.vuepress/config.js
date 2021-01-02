db1d2eee1d8fe6d4e1e6fa0eb2daa369c20b747e
zq99299 <99299684@qq.com>
2021-01-02 19:05:03 +0800

    升级到 1.7.1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -77,10 +77,12 @@ module.exports = {
       transformer: (timestamp, lang) => {
         // 不要忘了安装 moment
         const moment = require('moment')
-        moment.locale(lang)
-        return moment(timestamp).fromNow()
+        // moment.locale(lang)
+        // return moment(timestamp).fromNow()
+        return moment(timestamp).format('YYYY-MM-DD HH:mm:ss')
       }
-    }
-    ]
+    },
+    ],
+    ['vuepress-plugin-tags']
   ]
 }
